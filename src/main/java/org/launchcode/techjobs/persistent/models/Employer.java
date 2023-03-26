8f8698fc8f3a11feabb4045dc5bbe66c9e01ea42
Nolan <ndietz2121@gmail.com>
2023-03-26 13:40:12 -0500

    Part 2 complete(test task 1 and 2 all passing)

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,41 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @NotNull
+    @Size(max=75, message = "location should be less than 75 characters")
+    private String location;
 
+
+    @OneToMany
+    @JoinColumn(name ="employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer(){
+
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 }
